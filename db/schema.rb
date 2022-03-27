6dbaabd898773ef1f0c7a37dfc1a53bd43e2dd1e
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-03-28 03:21:29 +0900

    10章【投稿機能の作成(3) - 投稿できるようにしよう】

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_041348) do
+ActiveRecord::Schema.define(version: 2022_03_27_180638) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2022_03_27_041348) do
   create_table "post_images", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
